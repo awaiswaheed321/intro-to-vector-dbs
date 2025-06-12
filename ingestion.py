8dd374ecc3118cf05315e5e6c02342b35cc04ac2
Awais Waheed <awaiswaheed321@gmail.com>
2025-06-12 12:25:09 -0500

    Injestion

---
+++ ingestion.py
@@ -0,0 +1,24 @@
+import os
+from dotenv import load_dotenv
+from langchain_community.document_loaders import TextLoader
+from langchain_text_splitters import CharacterTextSplitter
+from langchain_openai import OpenAIEmbeddings
+from langchain_pinecone import PineconeVectorStore
+
+load_dotenv()
+
+if __name__ == '__main__':
+    print('*****Starting...')
+    loader = TextLoader(file_path="blog.txt", encoding='utf-8')
+    document = loader.load()
+    print("\n*****Splitting")
+    text_splitter = CharacterTextSplitter(chunk_size=1000, chunk_overlap=0)
+    texts = text_splitter.split_documents(document)
+    print(f"document split into {len(texts)} chunks")
+
+    embeddings = OpenAIEmbeddings(api_key=os.environ.get("OPENAI_API_KEY"))
+
+    print("\n*****Ingestion")
+    PineconeVectorStore.from_documents(documents=texts, embedding=embeddings, index_name=os.environ.get("PINECONE_INDEX_NAME"))
+
+    print("\n*****Finished")
